a151abfa76233ad26ad54472fbfc0fbdf51e1f29
saied89 <saiedmomen@gmail.com>
2025-02-02 23:42:18 +0100

    small improvement

--- components-wireframe/src/main/java/dev/saied/components/wireframe/Switch.kt
+++ components-wireframe/src/main/java/dev/saied/components/wireframe/Switch.kt
@@ -1,26 +1,17 @@
 package dev.saied.components.wireframe
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.saied.components.core.BasicSwitch
 import dev.saied.components.wireframe.theme.ColorTokens
-import kotlinx.coroutines.FlowPreview
 
 // TODO: Add thump content
 @Composable
@@ -40,13 +31,13 @@ fun Switch(
                 shape = RoundedCornerShape(7.dp)
             )
             .size(width = 34.dp, height = 14.dp)
-            .requiredSize(width = 34.dp, height = 20.dp),
+            .requiredHeight(20.dp),
         checked = checked,
         onCheckedChange = onCheckedChanged,
         thumb = {
             Box(
                 modifier = Modifier
-                    .requiredSize(20.dp)
+                    .size(20.dp)
                     .background(
                         SwitchDefaults.thumbColor(enabled = enabled, checked = checked),
                         shape = CircleShape
